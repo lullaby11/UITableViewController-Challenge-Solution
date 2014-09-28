5897d943d8dccaab2479b2804d3d85558b0d66e7
Juan Manuel Marín Cobo <juanmanuelmarincobo@iMac-de-Sergio.local>
2014-09-28 14:24:53 +0200

    solved the challenge

--- UITableViewController Challenge Solution/CCTableViewController.h
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Juan Manuel Marín Cobo on 28/09/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
