5c06859c8ec45b39359ffaf9ea754577ff312791
Juan Manuel Marín Cobo <juanmanuelmarincobo@iMac-de-Sergio.local>
2014-09-28 13:51:16 +0200

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Juan Manuel Marín Cobo on 28/09/14.
+//  Copyright (c) 2014 Juan Manuel Marín Cobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Juan Manuel Marín Cobo on 28/09/14.
+//  Copyright (c) 2014 Juan Manuel Marín Cobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
